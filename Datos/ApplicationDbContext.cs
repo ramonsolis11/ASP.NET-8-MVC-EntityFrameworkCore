d392971cd28bcb8735e73728dc5444cf990b6eb3
Ramón Solís Núñez <ramonsolis11@gmail.com>
2025-01-10 15:02:55 -0600

    add files

---
+++ Datos/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CrudNet8MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudNet8MVC.Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        //Agregar los modelos aqui ( Cada modelo corresponde a una tabla en la BD)
+        public DbSet<Contacto> Contacto { get; set; }
+    }
+}
